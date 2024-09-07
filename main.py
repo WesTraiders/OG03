e5d9b1dc6b3de13c7b8bd4094e6f7176d2ae1833
WesTraiders <baryshnikov.ilya@gmail.com>
2024-09-07 22:15:08 +0200

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.examples.aliens import SCREENRECT
 
 pygame.init()
@@ -8,7 +9,19 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Тир")
-icon = pygame.image.load("icon.png")
+icon = pygame.image.load("img/1620460.png")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("img/target.png")
+target_widht = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_widht)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
 
 running = True
 while running:
