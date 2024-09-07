92f4bcfd661844f0be2a70c1428adb3d754c5bb6
WesTraiders <baryshnikov.ilya@gmail.com>
2024-09-07 22:00:55 +0200

    add const

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
+from pygame.examples.aliens import SCREENRECT
 
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Тир")
+icon = pygame.image.load("icon.png")
 
 running = True
 while running:
